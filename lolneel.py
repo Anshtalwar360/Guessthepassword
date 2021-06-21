19954dbde0132cc424f56e7b59f11756237c17c0
aNSH <74968124+Anshtalwar360@users.noreply.github.com>
2021-06-21 11:28:48 +0400

    Add files via upload

---
+++ lolneel.py
@@ -0,0 +1,8 @@
+import random
+
+words=['sink','dunk','last','idle','bsdk','math','lmao','town','life']
+
+for x in range(0,len(words)):
+	x=random.choice(words)
+
+
---
+++ neel.py
@@ -0,0 +1,44 @@
+import color
+import lolneel
+import time
+import os
+
+
+secret_word = lolneel.x
+guess= " "
+countb=0
+countc=0
+countg=0
+
+while guess != secret_word:
+    guess = input("Enter your guess: ")
+
+    if len(guess)>len(secret_word):
+    	print("the length of the guess is greater than that of the secret word")
+    	continue
+            
+    elif len(guess)<len(secret_word):
+       	 print("the length of the guess is less than that of the secret word")
+       	 continue
+
+    for i in range(0,len(guess)):
+        
+        if guess[i] == secret_word[i]:
+            countb = countb+1
+
+
+    for i in range(len(guess)):
+      for j in range(len(guess)):
+        if(guess[i] == secret_word[j] and j != i): countc+=1
+
+    countg= countg+1   
+
+    print(countc,'cows', countb, 'bulls')
+    countc, countb = 0, 0
+print(color.BOLD + 'You won in',countg,'guesses'+ color.END)
+os.system("curl parrot.live")
+sys.exit()
+
+
+
+
