8ab4af8a73e4e988a34a3840d18c3d3be578cace
aNSH <74968124+Anshtalwar360@users.noreply.github.com>
2021-06-22 11:58:25 +0400

    Update lolneel.py

--- lolneel.py
+++ lolneel.py
@@ -1,6 +1,7 @@
 import random
 
-words=['sink','dunk','last','idle','bsdk','math','lmao','town','life']
+words=['sink','dunk','last','idle','math','lmao','town','life','king','shit', 'that','stat','melt','dumb','salt','cafe','sour'
+       ,'link','word','halt','dogs','doge','gasp','rant','pant','tour','play','game','home','dice','vile','vice','mean','lose','four']
 
 for x in range(0,len(words)):
 	x=random.choice(words)
